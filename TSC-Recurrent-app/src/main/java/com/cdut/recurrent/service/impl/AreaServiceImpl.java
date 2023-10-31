7914553393f7cddacc3fdd5858ffad5945ea6453
wyr <2521589240@qq.com>
2023-10-31 21:16:23 +0800

    代码优化

--- TSC-Recurrent-app/src/main/java/com/cdut/recurrent/service/impl/AreaServiceImpl.java
+++ TSC-Recurrent-app/src/main/java/com/cdut/recurrent/service/impl/AreaServiceImpl.java
@@ -2,18 +2,14 @@ package com.cdut.recurrent.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cdut.current.entity.Area;
-import com.cdut.current.entity.Student;
 import com.cdut.current.vo.AreaVO;
 import com.cdut.recurrent.mapper.AreaMapper;
-import com.cdut.recurrent.mapper.StudentMapper;
 import com.cdut.recurrent.service.IAreaService;
-import com.cdut.recurrent.service.IStudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
 import java.util.*;
-import java.util.function.BinaryOperator;
 import java.util.stream.Collectors;
 
 @Service
