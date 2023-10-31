2f22aea9ae79d0757efb21ee43e3d1a7e6e822fa
zhaoli <3030486865@qq.com>
2023-10-31 16:02:35 +0800

    feat: 添加统一的异常处理

---
+++ TSC-Recurrent-api/src/main/java/com/cdut/current/exception/AppException.java
@@ -0,0 +1,56 @@
+package com.cdut.current.exception;
+
+public class AppException extends RuntimeException {
+    public AppException() {
+    }
+
+    public AppException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AppException(String message) {
+        super(message);
+    }
+
+    public AppException(Throwable cause) {
+        super(cause);
+    }
+
+    public AppException(String message, Object... args) {
+        super(format(message, args));
+    }
+
+    public static String format(String template, Object... args) {
+        template = String.valueOf(template);
+        StringBuilder builder = new StringBuilder(template.length() + 16 * args.length);
+        int templateStart = 0;
+
+        int i;
+        int placeholderStart;
+        for(i = 0; i < args.length; templateStart = placeholderStart + 2) {
+            placeholderStart = template.indexOf("%s", templateStart);
+            if (placeholderStart == -1) {
+                break;
+            }
+
+            builder.append(template, templateStart, placeholderStart);
+            builder.append(args[i++]);
+        }
+
+        builder.append(template.substring(templateStart));
+        if (i < args.length) {
+            builder.append(" [");
+            builder.append(args[i++]);
+
+            while(i < args.length) {
+                builder.append(", ");
+                builder.append(args[i++]);
+            }
+
+            builder.append(']');
+        }
+
+        return builder.toString();
+    }
+
+}
