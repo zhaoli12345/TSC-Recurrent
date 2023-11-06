7ee9a4f7ceefde5630ae7629c70e86c8e64e9a9d
zhaoli <3030486865@qq.com>
2023-11-06 09:28:45 +0800

    feat: 根据地区查询岩性信息

--- TSC-Recurrent-app/src/main/java/com/cdut/recurrent/controller/OutputController.java
+++ TSC-Recurrent-app/src/main/java/com/cdut/recurrent/controller/OutputController.java
@@ -1,6 +1,8 @@
 package com.cdut.recurrent.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.cdut.current.common.ServiceResult;
+import com.cdut.current.entity.Output;
 import com.cdut.recurrent.service.IOutputService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +23,13 @@ public class OutputController {
      */
     @RequestMapping(value = "/calculateAgeById/{id}", method = RequestMethod.GET)
     public ServiceResult<Float> calculateAgeById(@PathVariable Long id) {
-        return ServiceResult.success(outputService.calculateAgeById(id,outputService));
+        return ServiceResult.success(outputService.calculateAgeById(id, outputService));
+    }
+
+    @RequestMapping(value = "/findByAreaId/{areaId}", method = RequestMethod.GET)
+    public ServiceResult<List<Output>> findByAreaId(@PathVariable Long areaId) {
+        QueryWrapper<Output> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("area_id", areaId);
+        return ServiceResult.success(outputService.list(queryWrapper));
     }
 }
