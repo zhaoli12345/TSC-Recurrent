f1c44df5b3d41b5c23c8133d3ff9a4f2e761e49d
zhaoli <3030486865@qq.com>
2023-11-15 11:06:16 +0800

    fix: bug小数转化字符串

--- TSC-Recurrent-api/src/test/java/com/cdut/current/pattern/PatternTest.java
+++ TSC-Recurrent-api/src/test/java/com/cdut/current/pattern/PatternTest.java
@@ -7,13 +7,12 @@ import java.util.*;
 public class PatternTest {
     @Test
     public void testPattern() {
-        String formula = "${1}-0.9*(${1}-${2})";
+        String formula = "${1}";
 
         List<Long> ids = PatternUtil.getIdsFromFormula(formula);
         System.out.println(ids);
         Map<Long, Float> agesMap = new HashMap<>();
-        agesMap.put(1L, 5.335f);
-        agesMap.put(2L, 2.58f);
+        agesMap.put(1L, 0.00011f);
 
         String infixExpression = PatternUtil.changeFormula(agesMap, formula);
         System.out.println(infixExpression);
